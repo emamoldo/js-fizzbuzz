7bd52fe40f7a72aa2dcd955906ee1e4beccbbd3b
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-21 14:59:32 +0100

    Add Title and List in HTML, Add Cicle For in JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
+// Creating a for cicle with numbers that goes from 1 to 100;
 
+const elementList=document.getElementById("list")
+
+for (let i=1; i<=14; i++) {
+    console.log(i);
+        elementList.insertAdjacentHTML('beforeend', `<li class="box">${i}</li>` )
+}
