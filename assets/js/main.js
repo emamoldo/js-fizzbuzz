1fb24bd2c237183819a329ba859808fac9a23632
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-21 18:12:08 +0100

    Add to elementList the names and colors, Add Css styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,28 @@
 // Cicle that goes from 1 to 100;
 const elementList = document.getElementById("list");
 
-for (let i = 1; i <= 35; i++) {
 
+for (let i = 1; i <= 100; i++) {
 
-    elementList.insertAdjacentHTML('beforeend', `<li class="box">${i}</li>`);
-
+    
+    
+    
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz");
+        elementList.insertAdjacentHTML('beforeend', `<li class="box fizzbuzz">FizzBuzz</li>`);
 
     } else if (i % 3 == 0) {
         console.log("Fizz");
+        elementList.insertAdjacentHTML('beforeend', `<li class="box fizz">Fizz</li>`);
+
     } else if (i % 5 == 0) {
         console.log("Buzz");
+        elementList.insertAdjacentHTML('beforeend', `<li class="box buzz">Buzz</li>`);
+
     } else {
         console.log(i);
+        elementList.insertAdjacentHTML('beforeend', `<li class="box ${i}">${i}</li>`);
+
     }
     
 }
